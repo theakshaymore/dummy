346a5524a9e3c4a153d94e8dc4416513bddf530e
Akshay More <moreakshay725@gmail.com>
2025-01-03 18:18:27 +0530

    update server.js to handle routing for root and login pages, and improve server log message

--- nodeNginx/server.js
+++ nodeNginx/server.js
@@ -3,10 +3,24 @@ const fs = require("fs");
 const path = require("path");
 
 const port = 3000;
+const hostName = "127.0.0.1";
+
 const server = http.createServer((req, res) => {
-  res.data("ssss");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("welcome to root page");
+  } else if (req.url === "/login") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("welcome to login page");
+  } else {
+    res.statusCode = 400;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 not found");
+  }
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://'${hostName}:${port}'`);
 });
